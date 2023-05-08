4b49436d673ffc73cb3498bafe7cae24327adea3
ansen01-svg <ansenbeyc73@gmail.com>
2023-05-08 11:38:00 +0530

    configured front end

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    // devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
+        compress: true,
+        port: 3001,
+    },
+}
